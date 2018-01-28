6ccad1167aee70aef3bb799cb1b3444f9c158b65
machaerus <machaerus@protonmail.com>
2018-01-28 22:03:33 +0100

    changed from Scopus to CrossRef searching

--- ugarit_cli.py
+++ ugarit_cli.py
@@ -6,62 +6,95 @@ import requests
 import json
 import re
 
+"""
+
+@author: machaerus
+
+TODO:
+	- add timeouts
+	- implement retries where missing
+	- allow to download more than 20 items
+
+"""
 
 class Ugarit():
 
 	def __init__(self):
 
 		# constants
-		self.RES_LIMIT = 3
-		self.scopus_url = "http://api.elsevier.com/content/search/scopus"
-		self.scopus_key = '23096dfc3e1a8d87504a227fb4314c9f'
-		self.scopus_view = "STANDARD"
+		self.TRIALS = 3
 
 		# init
 		self.sh = SciHub()
 		self.cr = Crossref()
 
-	def scopus_submit(self, author, phrase):
-		query = re.sub("\s+", "\+", phrase)
-		if author != "":
-			author = "AUTH%28{}%29".format(re.sub("\s+", "\+", author))
-			query = "{}+{}".format(author, query)
-		r = requests.get("{}?query={}&apiKey={}&view={}&count={}".format(
-				self.scopus_url, 
-				query, 
-				self.scopus_key, 
-				self.scopus_view, 
-				self.RES_LIMIT
-		))
-		self.scopus_res = r.json()
-		self.scopus_res = self.scopus_res['search-results']['entry']
-		return self.scopus_res
-
-		#data = scopus_submit(author, phrase)
-		#doi = data['search-results']['entry'][6]['prism:doi']
+	def crossref_search(self, query, offset):
+		print("Searching...")
+		trial = 0
+		while trial < self.TRIALS:
+			try:
+				r = self.cr.works(query=query, offset=offset)
+			except Exception:
+				print("Error while calling API. Retrying...")
+				trial += 1
+				time.sleep(1)
+				continue
+			if r['status'] == "ok":
+				results = r['message']['items']
+				results = [item for item in results if
+								"DOI" in item.keys() and
+								"title" in item.keys() and
+								"author" in item.keys()]
+				if offset == 0:
+					self.search_results = results
+				else:
+					self.search_results += results
+				return True
+			else:
+				print("Incorrect server response. Retrying...")
+				trial += 1
+				time.sleep(1)
+				continue
+		print("Search failed. Something's broken.")
+		return False
 
 	def set_doi(self, doi):
 		self.doi = doi
 
 	def get_doi(self, n):
-		self.doi = self.scopus_res[n]['prism:doi']
+		self.doi = self.search_results[n]['DOI']
 		return self.doi
 
-	def print_scopus_results(self):
-		print("Number of articles found: {}".format(len(self.scopus_res)))
+	def print_search_results(self):
+		print("Number of articles found: {}".format(len(self.search_results)))
 		print()
-		for article in self.scopus_res:
+		for article in self.search_results:
 
-			title = article['dc:title']
-			if len(title) > 50:
-				title = title[:50] + " . . ."
+			title = article['title'][0]
+			if len(title) > 60:
+				title = title[:60] + " . . ."
 
-			print("[#{}]".format(self.scopus_res.index(article)))
+			try:
+				author = ""
+				for a in article['author']:
+					author += "{}, {}; ".format(a['family'], a['given'])
+				author = author[:-2]
+			except Exception as e:
+				print(article)
+				raise
+
+
+			if article['issued']['date-parts'][0][0] is not None:	
+				date = article['issued']['date-parts'][0][0]
+			else:
+				date = article['created']['date-parts'][0][0]
+
+			print("[#{}]".format(self.search_results.index(article)))
 			print(" |    {:30}{}".format("Title:",					title))
-			print(" |    {:30}{}".format("Author:",					article['dc:creator']))
-			print(" |    {:30}{}".format("Date:",					article['prism:coverDisplayDate']))
-			print(" |    {:30}{}".format("Publication name:",		article['prism:publicationName']))
-			print(" |    {:30}{}".format("Type:",					article['subtypeDescription']))
+			print(" |    {:30}{}".format("Author:",					author))
+			print(" |    {:30}{}".format("Date:",					date))
+			print(" |    {:30}{}".format("Publisher:",				article['publisher']))
+			print(" |    {:30}{}".format("Type:",					article['type']))
 			print()
 
 
@@ -70,7 +103,7 @@ class Ugarit():
 		response = r.json()
 		return response['is_oa']
 	
-	def unpaywall_download(self, doi, save="test.pdf"):
+	def unpaywall_download(self, doi, save="test2.pdf"):
 		# https://stackoverflow.com/questions/15644964/python-progress-bar-and-downloads
 		r = requests.get("https://api.oadoi.org/v2/{}?email=test@example.com".format(doi))
 		response = r.json()
@@ -92,7 +125,7 @@ class Ugarit():
 			return False
 		return True
 
-	def crossref_get(self, doi):
+	def crossref_get_citation(self, doi):
 		return cn.content_negotiation(ids=doi, format="bibentry")
 
 
@@ -104,17 +137,26 @@ print("Search for a new article.")
 print("-------------------------")
 print()
 
-author = input("{:20}".format("Author (optional):"))
-keyphrase = input("{:20}".format("Query (mandatory):"))
+query = input("{:20}".format("Query:"))
 
 print()
 
-ugarit.scopus_submit(author, keyphrase)
-ugarit.print_scopus_results()
+offset = 0
+while True:
+	ugarit.crossref_search(query, offset)
+	ugarit.print_search_results()
+	print()
+	load_more = input("Load more results? [y/n] ")
+	if load_more == "n":
+		break
+	elif load_more == "y":
+		offset += 20
+		continue
+	break
 
 print()
 
-article_no = input("Select article (0-{}): ".format(ugarit.RES_LIMIT-1))
+article_no = input("Select article: ")
 doi = ugarit.get_doi(int(article_no))
 
 print()
@@ -123,7 +165,7 @@ check_unpaywall = ugarit.unpaywall_check(doi)
 if check_unpaywall:
 	input = input("Article found on Unpaywall. Do you want to download? [y/n] ")
 	if input == "y":
-		print("Downloading...")
+		print("Downloading (a little patience please)...")
 		ugarit.unpaywall_download(doi)
 		print("Done!")
 	elif input == "n":
@@ -131,7 +173,7 @@ if check_unpaywall:
 else:
 	input = input("Article is not accessible for free. Do you want to try SciHub? [y/n] ")
 	if input == "y":
-		print("Downloading...")
+		print("Downloading (a little patience please)...")
 		if ugarit.scihub_download(doi):
 			print("Done!")
 		else: 
